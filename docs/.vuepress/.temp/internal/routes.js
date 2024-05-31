6e9bf0ca6d54cc6ae7af0ec54fb02f37a1005288
15911075183ma <141103706+15911075183ma@users.noreply.github.com>
2024-05-31 14:52:31 +0800

    修改favicon图片

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -4,20 +4,3 @@ export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/mazepeng/WebstormProjects/zhuwei-doc/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"首页"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/mazepeng/WebstormProjects/zhuwei-doc/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
